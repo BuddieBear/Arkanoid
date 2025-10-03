201974f6d75c4a8bf75b5a17462596d3a3b0a4d8
Dang <25haidang@gmail.com>
2025-10-03 22:44:03 +0700

    add Brick + PowerUp

---
+++ src/main/java/uet/project/arkanoid/GameObject.java
@@ -0,0 +1,21 @@
+package uet.project.arkanoid;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public abstract class GameObject {
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+
+    public GameObject(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void update();
+    public abstract void render(/*GraphicsContext gc*/);
+
+}

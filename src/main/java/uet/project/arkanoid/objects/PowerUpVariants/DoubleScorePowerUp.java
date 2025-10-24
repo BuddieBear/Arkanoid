22ca9f4c68cfc82835fc5d25adc3127a4addb092
24020210 Nguyễn Thành Long <nguyenthanhlong23102006ab@gmail.com>
2025-10-24 14:39:24 +0700

    Fear(PowerUp): add power up for objective

---
+++ src/main/java/uet/project/arkanoid/objects/PowerUpVariants/DoubleScorePowerUp.java
@@ -0,0 +1,23 @@
+package uet.project.arkanoid.objects.PowerUpVariants;
+
+import uet.project.arkanoid.game.GameSetup;
+import uet.project.arkanoid.objects.GameObject;
+import uet.project.arkanoid.utils.Basis;
+import uet.project.arkanoid.objects.PowerUp;
+
+public class DoubleScorePowerUp extends PowerUp {
+
+    public DoubleScorePowerUp(GameObject object, double width, double height, GameSetup stage) {
+        super(object, width, height, stage);
+        this.type = PowerUpType.DOUBLE_SCORE;
+    }
+
+    @Override
+    public void applyEffect() {
+        stage.addScore(stage.getScore());
+    }
+
+    @Override
+    public void removeEffect() {
+    }
+}

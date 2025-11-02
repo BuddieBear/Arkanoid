976d7210c291ea9d2415dcc88f891c4b2caec695
Hoàng Trung Dũng <lordlightning2019@gmail.com>
2025-11-02 14:00:39 +0700

    refactor (Menu): Reorganize button orders.

--- src/main/java/uet/project/arkanoid/ui/MenuView.java
+++ src/main/java/uet/project/arkanoid/ui/MenuView.java
@@ -9,25 +9,25 @@ import uet.project.arkanoid.utils.Basis;
 import uet.project.arkanoid.base.*;
 
 public class MenuView implements View {
-    private static final Rectangle PLAY_BUTTON = new Rectangle(
+    private static final Rectangle SETTING_BUTTON = new Rectangle(
             new Point(Basis.PLAY_X + Basis.PLAY_W / 2.0, Basis.PLAY_Y + Basis.PLAY_H / 2.0),
             new Vector2D(Basis.PLAY_W, Basis.PLAY_H),
             0
     );
 
-    private static final Rectangle INSTRUCTION_BUTTON = new Rectangle(
+    private static final Rectangle LOAD_GAME_BUTTON = new Rectangle(
             new Point(Basis.INSTRUCTION_X + Basis.INSTRUCTION_W / 2.0, Basis.INSTRUCTION_Y + Basis.INSTRUCTION_H / 2.0),
             new Vector2D(Basis.INSTRUCTION_W, Basis.INSTRUCTION_H),
             0
     );
 
-    private static final Rectangle SETTING_BUTTON = new Rectangle(
+    private static final Rectangle PLAY_BUTTON = new Rectangle(
             new Point(Basis.SETTING_X + Basis.SETTING_W / 2.0, Basis.SETTING_Y + Basis.SETTING_H / 2.0),
             new Vector2D(Basis.SETTING_W, Basis.SETTING_H),
             0
     );
 
-    private static final Rectangle LOAD_GAME_BUTTON = new Rectangle(
+    private static final Rectangle INSTRUCTION_BUTTON = new Rectangle(
             new Point(Basis.LOAD_GAME_BTN_X + Basis.LOAD_GAME_BTN_W / 2.0, Basis.LOAD_GAME_BTN_Y + Basis.LOAD_GAME_BTN_H / 2.0),
             new Vector2D(Basis.LOAD_GAME_BTN_W, Basis.LOAD_GAME_BTN_H),
             0
@@ -54,15 +54,17 @@ public class MenuView implements View {
         gc.drawImage(Basis.MENU, 0, 0, Basis.SCREEN_WIDTH, Basis.SCREEN_HEIGHT);
 
         // play button
-        gc.drawImage(Basis.PLAY_BUTTON, Basis.PLAY_X, Basis.PLAY_Y, Basis.PLAY_W, Basis.PLAY_H);
-
-        // option button
-        gc.drawImage(Basis.INSTRUCTION_BUTTON, Basis.INSTRUCTION_X, Basis.INSTRUCTION_Y, Basis.INSTRUCTION_W, Basis.INSTRUCTION_H);
-
-        // setting button
-        gc.drawImage(Basis.SETTING_BUTTON, Basis.SETTING_X, Basis.SETTING_Y, Basis.SETTING_W, Basis.SETTING_H);
+        gc.drawImage(Basis.PLAY_BUTTON, Basis.SETTING_X, Basis.SETTING_Y, Basis.SETTING_W, Basis.SETTING_H);
 
         // load game button
-        gc.drawImage(Basis.LOAD_GAME_BUTTON, Basis.LOAD_GAME_BTN_X, Basis.LOAD_GAME_BTN_Y, Basis.LOAD_GAME_BTN_W, Basis.LOAD_GAME_BTN_H);
+        gc.drawImage(Basis.INSTRUCTION_BUTTON, Basis.LOAD_GAME_BTN_X, Basis.LOAD_GAME_BTN_Y, Basis.LOAD_GAME_BTN_W, Basis.LOAD_GAME_BTN_H);
+
+        // option button
+        gc.drawImage(Basis.LOAD_GAME_BUTTON, Basis.INSTRUCTION_X, Basis.INSTRUCTION_Y, Basis.INSTRUCTION_W, Basis.INSTRUCTION_H);
+
+        // setting button
+        gc.drawImage(Basis.SETTING_BUTTON, Basis.PLAY_X, Basis.PLAY_Y, Basis.PLAY_W, Basis.PLAY_H);
+
+
     }
 }
